f876d161968cd673af7276c71981ac92ca022403
koassumo <0770407@gmail.com>
2019-12-23 03:01:06 +0300

    сделано ДЗ урок 2 (улучшено)

--- src/lesson2/CheckAndSumArray.java
+++ src/lesson2/CheckAndSumArray.java
@@ -9,19 +9,30 @@ public class CheckAndSumArray {
         if (myArray.length != 4) throw new MyArraySizeException (numberArray);      // проверка размерности (количества строк)
 
         int sum = 0;
-        for (String[] row : myArray) {
-            if (row.length != 4) throw new MyArraySizeException(numberArray);       // проверка размерности (элементов в каждой строке)
-            for (String element : row) {
+        for (int row = 0; row < myArray.length; row++) {
+            if (myArray[row].length != 4) throw new MyArraySizeException(numberArray);       // проверка размерности (элементов в каждой строке)
+            for (int element = 0; element < myArray[row].length; element++) {
                 try {
-                    sum += Integer.parseInt(element);
+                    sum += Integer.parseInt(myArray[row][element]);
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException (numberArray);
+                    throw new MyArrayDataException (numberArray, row, element);
                 }
             }
         }
-
-        System.out.println("Массив " + numberArray + ": ок. Результат суммирования = " + sum);
+        System.out.println("Массив " + (numberArray+1) + ": ок. Результат суммирования = " + sum);
     }
-
 }
 
+
+
+//        БЕЗ ОПРЕДЕЛЕНИЯ НЕКОРРЕКТНОГО ЭЛЕМЕНТА
+//        for (String[] row : myArray) {
+//            if (row.length != 4) throw new MyArraySizeException(numberArray);       // проверка размерности (элементов в каждой строке)
+//            for (String element : row) {
+//                try {
+//                    sum += Integer.parseInt(element);
+//                } catch (NumberFormatException e) {
+//                    throw new MyArrayDataException (numberArray);
+//                }
+//            }
+//        }
--- src/lesson2/MyArraySizeException.java
+++ src/lesson2/MyArraySizeException.java
@@ -3,7 +3,7 @@ package lesson2;
 public class MyArraySizeException extends Exception{
 
     public MyArraySizeException(int numberArray) {
-        System.out.println("Массив " + numberArray + " имеет неверную размерность");
+        System.out.println("Массив " + (numberArray+1) + " имеет неверную размерность");
     }
 
 }
